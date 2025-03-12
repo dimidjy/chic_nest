1f465d9ed52aae6e26e4037823c5199ae1deb735
dkolinchuk <d.kolinchuk@ydt-global.com>
2025-03-12 12:54:07 +0200

    Init drupal 10 changes

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -286,7 +286,7 @@ $databases = [];
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'z3Qzrj56MUeRE3hXZzK6N6DSIxGo0auDqlB3sQSdUCU-i3MVw1o8zK6oEDeSZA_VDQ3ArTlg-Q';
 
 /**
  * Deployment identifier.
@@ -892,3 +892,16 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+$databases['default']['default'] = array (
+  'database' => 'drupal10',
+  'username' => 'drupal10',
+  'password' => 'drupal10',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['config_sync_directory'] = 'sites/default/files/config_FuViH4jBFHO9dRQNd6F_wcGXvtegai1Q5hJqZxcIbh4B7_24bdz8kECzU3hy9Hhna1gigm_i6A/sync';
